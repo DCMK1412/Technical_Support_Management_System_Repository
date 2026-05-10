25f99571b8435cbc902a680e5cc53082a8b7fd21
Jack Asaka <anasissa4100@gmail.com>
2026-05-10 14:47:54 +0300

    Complete database models: Ticket, Category, TicketComments, and TicketAttachments

--- tickets/models.py
+++ tickets/models.py
@@ -14,4 +14,67 @@ class User(AbstractUser):
     def __str__(self):
         return f"{self.username} - {self.role}"
     
+
+class Cateogry(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField(blank=True, null = True)
+    created_time= models.DateTimeField(auto_now_add= True) 
+
+    class Meta:
+        verbose_name_plural = "Categories"    #توحيد Categories مع Categorie
+
+    def __str__(self):
+        return self.name
     
+class Ticket(models.Model):
+    STATUS_CHOICES = (
+        ('OPEN', 'Open'),
+        ('IN_PROGRESS', 'In Progress'),
+        ('WAITING_FOR_USER','Waiting_for_user'),
+        ('RESOLVED', 'Resolved'),
+        ('CLOSED', 'Closed'),
+        )
+    PRIORITY_CHOICES = (
+        ('LOW', 'Low'),
+        ('MEDIUM', 'Medium'),
+        ('HIGH', 'High'),
+        ('URGENT', 'Urgent'),
+    )
+
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+
+    category = models.ForeignKey(Cateogry, on_delete=models.CASCADE, related_name='tickets')
+    created_by= models.ForeignKey(User, on_delete=models.CASCADE, related_name= 'created_tickets')
+    assigned_to = models.ForeignKey(User, on_delete=models.SET_NULL,null = True, blank=True, related_name='assigned_tickets')
+
+    status = models.CharField(max_length=20, choices= STATUS_CHOICES, default='OPEN')
+    priority = models.CharField(max_length=10, choices=PRIORITY_CHOICES, default='LOW')
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"Ticket #{self.id}: {self.title}"
+    
+    
+
+class TicketComment(models.Model):
+    ticket = models.ForeignKey(Ticket, on_delete=models.CASCADE, related_name='comments')
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    message = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Comment by {self.author.username} on {self.ticket.id}"
+
+
+
+class TicketAttachment(models.Model):
+    ticket = models.ForeignKey(Ticket, on_delete=models.CASCADE, related_name='attachments')
+    file = models.FileField(upload_to='ticket_attachments/')
+    uploaded_by = models.ForeignKey(User, on_delete=models.CASCADE)
+    uploaded_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Attachment for {self.ticket.id}"
