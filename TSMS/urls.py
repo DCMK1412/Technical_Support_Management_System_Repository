4d22606586aa0a4c53fde19b9b30952b0de049e2
Anas Issa <anasissa4100@gmail.com>
2026-07-31 20:50:34 +0300

    fix urls: remove duplicate routing and use root path for unique endpoints

--- TSMS/urls.py
+++ TSMS/urls.py
@@ -23,7 +23,6 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('tickets/', include(('tickets.urls', 'tickets'), namespace='tickets')),
-    path('api/', include('tickets.urls')),
 ]
 
 if settings.DEBUG:
